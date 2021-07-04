71120d2700025c580a050f19f31f67c2de92f37f
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-04 16:55:51 -0400

    Updates

---
+++ precache-manifest.e86c7e3f8cb270ac1767b4444dd12e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0ec7c7e0518fd2cebad194d3504808",
+    "url": "/reactColorShader/index.html"
+  },
+  {
+    "revision": "e42cd14e6ce33803cdf5",
+    "url": "/reactColorShader/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/reactColorShader/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/reactColorShader/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42cd14e6ce33803cdf5",
+    "url": "/reactColorShader/static/js/main.100d050e.chunk.js"
+  },
+  {
+    "revision": "9294146ef6c7991366a3",
+    "url": "/reactColorShader/static/js/runtime-main.a31e83f0.js"
+  }
+]);
